b73ed1b8ac91ba2a7410696ccc474b4aef627100
Flavio <flaviolago@Flavios-Mac-mini.local>
2021-08-05 08:41:05 -0700

    Add c6 folder

--- c5/test.py
+++ c5/test.py
@@ -1,4 +1,11 @@
-picnicItems = {'apples': 5, 'cups': 2}
-msg =  'I am bringing ' + str(picnicItems.get('eggs')) + ' eggs.'
-print(msg)
-print(1==1==1)
+
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start # of each line of text on the clipboard.
+import pyperclip
+text = pyperclip.paste()
+# Separate lines and add stars.
+lines = text.split('\n')
+for i in range(len(lines)): # loop through all indexes for "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+text = '\n'.join(lines)
+pyperclip.copy(text)
